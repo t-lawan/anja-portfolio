5610909888de2e2db236d31dbeba8c5d1390384e
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-07-29 09:16:07 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,27 @@
 module.exports = {
   siteMetadata: {
     title: `anja-portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "zs-OjEhs935eTN_ZRMepmcwXiYWkUdN42HxRL2I8CIo",
-      "spaceId": "oxetiwip2rs0"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-emotion", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "zs-OjEhs935eTN_ZRMepmcwXiYWkUdN42HxRL2I8CIo",
+        spaceId: "oxetiwip2rs0",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-emotion",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
